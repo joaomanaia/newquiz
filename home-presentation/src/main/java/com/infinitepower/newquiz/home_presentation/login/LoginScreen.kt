c65c9eeadbbbad40050f7a90cc17da22d4e76b1b
joaomanaia <joaopfmanaia@gmail.com>
2023-04-01 13:59:48 +0100

    Added google login only for devices using google play services #30

--- home-presentation/src/main/java/com/infinitepower/newquiz/home_presentation/login/LoginScreen.kt
+++ home-presentation/src/main/java/com/infinitepower/newquiz/home_presentation/login/LoginScreen.kt
@@ -1,6 +1,6 @@
 package com.infinitepower.newquiz.home_presentation.login
 
-import android.util.Log
+import android.content.Context
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -11,6 +11,8 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.infinitepower.newquiz.core.R
 import com.infinitepower.newquiz.core.analytics.logging.rememberCoreLoggingAnalytics
 import com.infinitepower.newquiz.online_services.core.worker.CheckUserDBWorker
@@ -36,9 +38,8 @@ fun LoginScreen(
 private fun LoginScreenImpl(
     onBack: () -> Unit
 ) {
-    val localContext = LocalContext.current
-
-    val workManager = remember { WorkManager.getInstance(localContext) }
+    val context = LocalContext.current
+    val workManager = remember { WorkManager.getInstance(context) }
 
     val firebaseLogin = rememberLauncherForActivityResult(contract = FirebaseAuthUIActivityResultContract()) {
         val request = OneTimeWorkRequestBuilder<CheckUserDBWorker>().build()
@@ -47,17 +48,27 @@ private fun LoginScreenImpl(
         onBack()
     }
 
+    val loginProviders = remember {
+        // If Google Play Services is available, we can use Google Sign In
+        if (isGooglePlayServicesAvailable(context)) {
+            listOf(
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder()
+                    .setRequireName(true)
+                    .build()
+            )
+        } else {
+            listOf(AuthUI.IdpConfig.EmailBuilder().build())
+        }
+    }
+
     val signInIntent = remember {
         AuthUI
             .getInstance()
             .createSignInIntentBuilder()
             .setTheme(R.style.Theme_NewQuiz)
-            .setAvailableProviders(
-                listOf(
-                    AuthUI.IdpConfig.GoogleBuilder().build(),
-                    AuthUI.IdpConfig.EmailBuilder().build()
-                )
-            ).setLogo(R.mipmap.ic_launcher_round)
+            .setAvailableProviders(loginProviders)
+            .setLogo(R.mipmap.ic_launcher_round)
             .setIsSmartLockEnabled(true)
             .setAlwaysShowSignInMethodScreen(true)
             .build()
@@ -66,4 +77,10 @@ private fun LoginScreenImpl(
     SideEffect {
         firebaseLogin.launch(signInIntent)
     }
+}
+
+private fun isGooglePlayServicesAvailable(context: Context): Boolean {
+    val googleApiAvailability = GoogleApiAvailability.getInstance()
+    val status = googleApiAvailability.isGooglePlayServicesAvailable(context)
+    return status == ConnectionResult.SUCCESS
 }
