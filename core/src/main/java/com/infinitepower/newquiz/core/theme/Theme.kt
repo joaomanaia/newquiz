b7c7f1407deae1db4c7e79b0be2745f6081e1398
joaomanaia <joaopfmanaia@gmail.com>
2023-05-20 12:16:04 +0100

    Change getWindowInsetsController deprecated function

--- core/src/main/java/com/infinitepower/newquiz/core/theme/Theme.kt
+++ core/src/main/java/com/infinitepower/newquiz/core/theme/Theme.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import com.google.android.material.color.MaterialColors
 
 private val LightThemeColors = lightColorScheme()
@@ -57,11 +57,16 @@ fun NewQuizTheme(
         darkTheme -> darkColorScheme
         else -> lightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
+
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            currentWindow.statusBarColor = colorScheme.primary.toArgb()
+
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
