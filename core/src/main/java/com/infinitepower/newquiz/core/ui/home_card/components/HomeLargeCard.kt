d699fcbce506180557671d0454bb04a16a92ca5f
joaomanaia <joaopfmanaia@gmail.com>
2023-08-19 14:00:18 +0100

    Fix home large card disabled colors

--- core/src/main/java/com/infinitepower/newquiz/core/ui/home_card/components/HomeLargeCard.kt
+++ core/src/main/java/com/infinitepower/newquiz/core/ui/home_card/components/HomeLargeCard.kt
@@ -1,21 +1,36 @@
 package com.infinitepower.newquiz.core.ui.home_card.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.infinitepower.newquiz.core.R
 import com.infinitepower.newquiz.core.common.annotation.compose.PreviewNightLight
+import com.infinitepower.newquiz.core.common.compose.preview.BooleanPreviewParameterProvider
 import com.infinitepower.newquiz.core.theme.NewQuizTheme
 import com.infinitepower.newquiz.core.theme.spacing
 import com.infinitepower.newquiz.core.ui.home_card.model.CardIcon
@@ -31,24 +46,17 @@ fun HomeLargeCard(
 
     val title = stringResource(id = data.title)
 
-    val containerColor = if (data.backgroundPrimary) {
-        MaterialTheme.colorScheme.primary
-    } else MaterialTheme.colorScheme.surfaceVariant
-
-    val contentColor = if (data.backgroundPrimary) {
-        MaterialTheme.colorScheme.onPrimary
-    } else MaterialTheme.colorScheme.onSurfaceVariant
-
-    val isInspectionMode = LocalInspectionMode.current
+    val cardColors = if (data.backgroundPrimary) {
+        getPrimaryCardColors()
+    } else {
+        CardDefaults.cardColors()
+    }
 
     Card(
         onClick = data.onClick,
-        enabled = data.enabled || isInspectionMode,
+        enabled = data.enabled,
         modifier = modifier,
-        colors = CardDefaults.cardColors(
-            containerColor = containerColor,
-            contentColor = contentColor,
-        )
+        colors = cardColors
     ) {
         Column(
             modifier = Modifier.padding(spaceMedium),
@@ -88,16 +96,51 @@ fun HomeLargeCard(
 }
 
 @Composable
+private fun getPrimaryCardColors(): CardColors = CardDefaults.cardColors(
+    containerColor = MaterialTheme.colorScheme.primary,
+    contentColor = MaterialTheme.colorScheme.onPrimary,
+    disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = DisabledAlpha),
+    disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = DisabledAlpha),
+)
+
+private const val DisabledAlpha = 0.38f
+
+@Composable
 @PreviewNightLight
 @OptIn(ExperimentalMaterial3Api::class)
-private fun HomeLargeCardPreview() {
+private fun HomeLargeCardPreview(
+    @PreviewParameter(BooleanPreviewParameterProvider::class) enabled: Boolean
+) {
     NewQuizTheme {
         Surface {
             HomeLargeCard(
                 data = HomeCardItem.LargeCard(
                     title = R.string.quick_quiz,
                     icon = CardIcon.Icon(Icons.Rounded.Check),
-                    onClick = {}
+                    onClick = {},
+                    enabled = enabled
+                ),
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+@PreviewNightLight
+@OptIn(ExperimentalMaterial3Api::class)
+private fun PrimaryHomeLargeCardPreview(
+    @PreviewParameter(BooleanPreviewParameterProvider::class) enabled: Boolean
+) {
+    NewQuizTheme {
+        Surface {
+            HomeLargeCard(
+                data = HomeCardItem.LargeCard(
+                    title = R.string.quick_quiz,
+                    icon = CardIcon.Icon(Icons.Rounded.Check),
+                    onClick = {},
+                    enabled = enabled,
+                    backgroundPrimary = true
                 ),
                 modifier = Modifier.padding(16.dp)
             )
