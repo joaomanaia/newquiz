2ee47279c5530692806383c0abd6eb3a3b2a95bd
joaomanaia <joaopfmanaia@gmail.com>
2023-09-01 10:46:34 +0100

    Fix build job action not applying gms

--- app/src/main/java/com/infinitepower/newquiz/initializer/firebase/CoreFirebaseInitializer.kt
+++ app/src/main/java/com/infinitepower/newquiz/initializer/firebase/CoreFirebaseInitializer.kt
@@ -5,14 +5,11 @@ import android.content.Context
 import androidx.startup.Initializer
 import com.google.firebase.FirebaseApp
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.app
 import com.google.firebase.ktx.initialize
 
 object CoreFirebaseInitializer : Initializer<FirebaseApp> {
     override fun create(context: Context): FirebaseApp {
-        Firebase.initialize(context)
-
-        return Firebase.app
+        return Firebase.initialize(context) ?: throw IllegalStateException("FirebaseApp is null")
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
