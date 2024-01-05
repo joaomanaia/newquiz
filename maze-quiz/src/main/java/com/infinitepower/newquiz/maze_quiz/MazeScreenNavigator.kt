c443f87eb4a8d6858dcd9f57d5332862aa82eff3
joaomanaia <joaopfmanaia@gmail.com>
2024-01-05 22:29:26 +0000

    Remove old maze-quiz module

--- maze-quiz/src/main/java/com/infinitepower/newquiz/maze_quiz/MazeScreenNavigator.kt
+++
@@ -1,7 +0,0 @@
-package com.infinitepower.newquiz.maze_quiz
-
-import com.infinitepower.newquiz.model.maze.MazeQuiz
-
-interface MazeScreenNavigator {
-    fun navigateToGame(item: MazeQuiz.MazeItem)
-}
