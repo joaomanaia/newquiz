0db6f672d9223032a1d2e1f33152011e24004f01
joaomanaia <joaopfmanaia@gmail.com>
2024-10-27 21:37:58 +0000

    Support question size to get questions from comparison quiz api

--- data/src/main/java/com/infinitepower/newquiz/data/repository/comparison_quiz/ComparisonQuizApiImpl.kt
+++ data/src/main/java/com/infinitepower/newquiz/data/repository/comparison_quiz/ComparisonQuizApiImpl.kt
@@ -6,10 +6,12 @@ import com.infinitepower.newquiz.domain.repository.CountryRepository
 import com.infinitepower.newquiz.model.comparison_quiz.ComparisonQuizCategory
 import io.ktor.client.HttpClient
 import io.ktor.client.request.headers
+import io.ktor.client.request.parameter
 import io.ktor.client.request.request
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -25,6 +27,11 @@ class ComparisonQuizApiImpl @Inject constructor(
 
         private const val COUNTRY_POPULATION_CATEGORY_ID = "country-population"
         private const val COUNTRY_AREA_CATEGORY_ID = "country-area"
+
+        internal val supportedLocalCategories = setOf(
+            COUNTRY_POPULATION_CATEGORY_ID,
+            COUNTRY_AREA_CATEGORY_ID
+        )
     }
 
     override suspend fun generateQuestions(
@@ -32,19 +39,23 @@ class ComparisonQuizApiImpl @Inject constructor(
         size: Int,
         random: Random
     ): List<ComparisonQuizItemEntity> {
-        return if (category.generateQuestionsLocally) {
+        return if (category.generateQuestionsLocally && category.id in supportedLocalCategories) {
             generateQuestionsLocally(
                 category = category,
                 size = size,
                 random = random
             )
         } else {
-            getQuestionsFromRemoteApi(category)
+            getQuestionsFromRemoteApi(
+                category = category,
+                size = size
+            )
         }
     }
 
     private suspend fun getQuestionsFromRemoteApi(
-        category: ComparisonQuizCategory
+        category: ComparisonQuizCategory,
+        size: Int
     ): List<ComparisonQuizItemEntity> {
         Log.d(TAG, "Getting questions from remote API with category: ${category.id}")
 
@@ -54,6 +65,8 @@ class ComparisonQuizApiImpl @Inject constructor(
             headers {
                 append(HttpHeaders.Accept, "application/json")
             }
+            method = HttpMethod.Get
+            parameter("size", size)
         }
 
         val textResponse = response.bodyAsText()
